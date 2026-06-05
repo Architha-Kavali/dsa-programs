4df98bd27f04ddefad459247da9b6484c188c1e1
Architha Kavali <archithakavali4@gmail.com>
2026-06-05 11:31:41 +0530

    Time: 9 ms (28.87%) | Memory: 19.2 MB (83.83%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumDifference(self, nums, k):
+        nums.sort()
+
+        ans = float('inf')
+
+        for i in range(len(nums) - k + 1):
+            diff = nums[i + k - 1] - nums[i]
+            ans = min(ans, diff)
+
+        return ans
